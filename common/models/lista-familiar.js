5fc7978ef152ca69848b7a5d00f8a45d12a60b77
IvanGonzalezMartin <kinokillo@gmail.com>
2017-11-21 10:36:17 +0100

    Propietario

--- common/models/lista-familiar.js
+++ common/models/lista-familiar.js
@@ -1,5 +1,10 @@
 'use strict';
 
 module.exports = function(Listafamiliar) {
-
+    
+    Listafamiliar.beforeRemote('create' , function(context, Listafamiliar ,next){
+        context.args.data.owner= context.req.accessToken.userId;
+        next();
+    });
+    
 };
--- server/boot/migration.js
+++ server/boot/migration.js
@@ -5,9 +5,6 @@ var config = require('../config.local.js')
 module.exports = function(app) {
     //data sources
     var mysqlDs = app.dataSources.mysqlDs;
-    var Usuario = app.models.Usuario;
-    var Role = app.models.Role;
-    var RoleMapping = app.models.RoleMapping;
     
     if(process.env.AUTOMIGRATE) {
         mysqlDs.automigrate(null, function (err) {
