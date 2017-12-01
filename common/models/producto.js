bdff8219714f2acee034192b975ea262b305aa39
IvanGonzalezMartin <kinokillo@gmail.com>
2017-12-01 08:34:46 +0100

    Poner productos de una lista a false

--- common/models/producto.js
+++ common/models/producto.js
@@ -1,5 +1,30 @@
 'use strict';
 
 module.exports = function(Producto) {
+ /**
+ * limpiar lista familiar
+ * @param {object} context contexto
+ * @param {Function(Error, array)} callback
+ */
 
-};
+        Producto.limpiarlista = function(context, callback) {
+            var array;
+            var usuario=Producto.app.models.Usuario;
+            var userid = context.req.accessToken.userId;
+            var lista;
+                usuario.findById(userid, function(err,productos) {
+                    lista=productos.listaFamiliarId;
+                     
+                      Producto.updateAll({listaFamiliarId: lista},{comprar:0},function(err, info){
+                        Producto.find({
+                            where:{listaFamiliarId:lista }},function (err,array){ 
+                            callback(null, array);                    
+                            });
+                          
+                      } 
+                      );
+        });
+    };
+
+}
+
