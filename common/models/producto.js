fc9f0aaafd74753e4b0e0477978d238681963f8d
IvanGonzalezMartin <kinokillo@gmail.com>
2017-12-05 10:47:33 +0100

    Un unico producto cambiado

--- common/models/producto.js
+++ common/models/producto.js
@@ -16,7 +16,7 @@ module.exports = function(Producto) {
                     lista=productos.listaFamiliarId;
                      
                       Producto.updateAll({listaFamiliarId: lista},{comprar:0},function(err, info){
-                        Producto.find({
+                        Producto.find({            
                             where:{listaFamiliarId:lista }},function (err,array){ 
                             callback(null, array);                    
                             });
@@ -25,6 +25,46 @@ module.exports = function(Producto) {
                       );
         });
     };
+ /**
+ * Comprar un unico producto
+ * @param {object} context 
+ * @param {Function(Error, array)} callback
+ */
+
+Producto.prototype.EliminarProducto = function(context, callback) {
+  var array;
+  var userid = context.req.accessToken.userId;
+  var usuario=Producto.app.models.Usuario;
+  var pid=this.listaFamiliarId;
+  var compra=this.comprar;
+  var lista;
+  var ides=this.id; 
+      usuario.findById(userid, function(err,productos) {
+            lista=productos.listaFamiliarId;
+             if(lista==pid){
+                 if(compra==0){
+                    Producto.updateAll({id: ides},{comprar:1},function(err, info){
+                        Producto.find({            
+                            where:{listaFamiliarId:lista }},function (err,array){ 
+                            callback(null, array);                    
+                            });
+                    }); 
+                 }else if(compra==1){
+                      Producto.updateAll({id: ides},{comprar:0},function(err, info){
+                        Producto.find({            
+                            where:{listaFamiliarId:lista }},function (err,array){ 
+                            callback(null, array);                    
+                            });
+                    }); 
+                 }
+             }else{
+                 
+                 console.log("no pertenece a la lista");
+                 
+             }
+                                       
+            });
+};
 
 }
 
